373b722f1b127d0ac9c7d35514e1fe5acddddc9a
carusina <79801539+carusina@users.noreply.github.com>
2025-05-21 19:10:50 +0900

    Delete test_neo4j.py

--- test_neo4j.py
+++
@@ -1,7 +0,0 @@
-# test_neo4j.py
-from models.knowledge_graph_service import KnowledgeGraphService
-
-if __name__ == "__main__":
-    kg = KnowledgeGraphService()
-    print("✔ Neo4j 연결 OK")
-    kg.close()

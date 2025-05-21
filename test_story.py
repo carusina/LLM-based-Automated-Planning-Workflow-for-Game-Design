15e87e99daed9439844d599939d3ca0b7dd47f03
carusina <79801539+carusina@users.noreply.github.com>
2025-05-21 19:10:38 +0900

    Delete test_story.py

--- test_story.py
+++
@@ -1,8 +0,0 @@
-# test_story.py
-from models.storyline_generator import StorylineGenerator
-
-if __name__ == "__main__":
-    stub_gdd = "커버 페이지\n내러티브 Overview 등..."
-    gen = StorylineGenerator()
-    story = gen.generate_storyline(stub_gdd, chapters=3)
-    print(story)

35fa922657124cf5db382088aff2bfe3d5489206
carusina <79801539+carusina@users.noreply.github.com>
2025-05-21 19:10:28 +0900

    Delete test_output.py

--- test_output.py
+++
@@ -1,7 +0,0 @@
-# test_output.py
-from models.document_generator import DocumentGenerator
-
-if __name__ == "__main__":
-    doc = DocumentGenerator()
-    path = doc.save_markdown("sample_gdd", "# 제목\n본문")
-    print("생성된 파일:", path)
